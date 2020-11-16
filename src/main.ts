3a2e5528f004fa48d627d90d2b59b08583b4efb3
David Solomon <dav.soli22@gmail.com>
2020-11-16 19:03:08 +0100

    Create App module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from 'modules/app';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.useGlobalFilters(
+  //   new MongoExceptionFilter(),
+  //   new MongooseExceptionFilter(),
+  //   new JwtExceptionFilter(),
+  // );
+  // const configService = app.get(ConfigService);
+  // const port = configService.get<number>('PORT');
   await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
