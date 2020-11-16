233216006067a5ef44499bbc3fc9b932bf75422f
David Solomon <dav.soli22@gmail.com>
2020-11-16 20:14:20 +0100

    Setup mongoose and config module

--- src/modules/app/index.ts
+++ src/modules/app/index.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from 'config';
 import { AppController } from './controllers';
 import { AppService } from './services';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: MongooseConfigService,
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MongooseConfigService],
 })
 export class AppModule {}
